b368fecc373a0968881e1f779ac9f9a3abe96220
gokulprathin8 <gokulprathin8@gmail.com>
2021-04-14 22:51:30 +0530

    Added Customized Error Response for User Entity's

--- src/main/java/com/github/gokulprathini8/demorestapis/user/UserResource.java
+++ src/main/java/com/github/gokulprathini8/demorestapis/user/UserResource.java
@@ -1,10 +1,12 @@
 package com.github.gokulprathini8.demorestapis.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,7 +22,18 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User getSingleUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id - " + id);
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity saveUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
---
+++ src/main/java/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,34 @@
+package exception;
+
+import com.github.gokulprathini8.demorestapis.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAppException(Exception ex, WebRequest request) throws Exception {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundExcpetion(UserNotFoundException ex, WebRequest request) throws Exception {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+
+    }
+
+}
